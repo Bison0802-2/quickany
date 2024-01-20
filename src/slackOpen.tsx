0ed614b1e5ccfd4a4498b2441d6a715e74e06215
Yuna Tomida <ytomida.mmm@gmail.com>
2024-01-21 01:25:48 +0900

    feat: generalize register

--- src/slackOpen.tsx
+++ src/slackOpen.tsx
@@ -1,4 +1,4 @@
-import OpenBase from "./components/openBase";
+import OpenBase from "./components/OpenBase";
 
 export default function Command() {
   return <OpenBase appName="slack" iconPath="slack-icon.png" />;
